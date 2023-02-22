e256a61cab1cf05fa122c6c3f3292d996b8a4905
pinoaudrey <pinoaudreyl@gmail.com>
2023-02-22 12:59:55 -0600

    getcwd/lookup

--- src/execute.c
+++ src/execute.c
@@ -23,17 +23,16 @@
 /***************************************************************************
  * Interface Functions
  ***************************************************************************/
+#define BSIZE 256
 
 // Return a string containing the current working directory.
 char* get_current_directory(bool* should_free) {
   // TODO: Get the current working directory. This will fix the prompt path.
   // HINT: This should be pretty simple
-  IMPLEMENT_ME();
-
   // Change this to true if necessary
-  *should_free = false;
+  *should_free = true;
 
-  return "get_current_directory()";
+  return getcwd(NULL, BSIZE);
 }
 
 // Returns the value of an environment variable env_var
@@ -42,12 +41,11 @@ const char* lookup_env(const char* env_var) {
   // to interpret variables from the command line and display the prompt
   // correctly
   // HINT: This should be pretty simple
-  IMPLEMENT_ME();
-
+const char* value = getenv(env_var);
   // TODO: Remove warning silencers
   (void) env_var; // Silence unused variable warning
 
-  return "???";
+  return value;
 }
 
 // Check the status of background jobs
